1d87044680c6e294587410aff718670c9a599ce5
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2022-07-27 13:08:39 +0900

    feat : 스태프 삭제 기능

--- pos-system-main/pos/server/Router/staff.js
+++ pos-system-main/pos/server/Router/staff.js
@@ -2,6 +2,33 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database");
 
+router.put("/Staff", (req, res) => {
+  const position = req.body.STAFF_POSITION;
+  const name = req.body.STAFF_NAME;
+  const number = req.body.STAFF_NUM;
+  const sqlQuery = `update board set(STAFF_POSITION, STAFF_NUM) where STAFF_NAME = ? `;
+  db.query(sqlQuery, [position, name, number], function (err, results, fields) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
+router.delete("/Staff/:name", (req, res) => {
+  const name = req.body.STAFF_NAME;
+  console.log(name);
+  const sqlQuery = `delete from staff where STAFF_NAME = ?`;
+
+  db.query(sqlQuery, [name], function (err, results, fields) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
 router.get("/Staff", (req, res) => {
   db.query("select * from staff", function (err, results, fields) {
     if (err) {
@@ -11,6 +38,7 @@ router.get("/Staff", (req, res) => {
     }
   });
 });
+
 router.post("/Staff", (req, res) => {
   const position = req.body.STAFF_POSITION;
   const name = req.body.STAFF_NAME;
--- pos-system-main/pos/server/server.js
+++ pos-system-main/pos/server/server.js
@@ -10,12 +10,14 @@ const db = require("./database");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
 //router folder
 const takeout = require("./Router/takeout");
 app.get("/category", takeout);
 app.all("/menu", takeout);
 const staff = require("./Router/staff");
 app.all("/Staff", staff);
+app.delete("/Staff/:name", staff);
 
 app.listen(port, () => {
   console.log(`connect at http://localhost:${port}`);
